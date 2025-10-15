0b25fa5b3aabf78a1a02c63ef55ec7e695135c36
Pontus Ingenius <Pontusingenius@gmail.com>
2025-10-15 11:57:12 +0200

    lagt till bild i footer med dblklick länk

---
+++ pontus.js
@@ -0,0 +1,6 @@
+const pontusBild = document.getElementById('pontusbild');
+
+pontusBild.addEventListener('dblclick', () => {
+    alert('Välkommen till min Github! https://github.com/Awreally ')
+    window.open('https://github.com/Awreally', '_blank');
+})
