5229018576847d72b9e4209ca213855f1158a0f2
Pontus Ingenius <Pontusingenius@gmail.com>
2025-10-15 14:20:54 +0200

    Meny fix

--- pontus.js
+++ pontus.js
@@ -1,4 +1,11 @@
 const pontusBild = document.getElementById('pontusbild');
+const navLinks = document.getElementById('menu');
+const menuIkon = document.getElementById('menuicon');
+
+menuIkon.addEventListener('click', () => {
+    menuIkon.classList.toggle('change');
+    navLinks.classList.toggle('open');
+})
 
 pontusBild.addEventListener('dblclick', () => {
     alert('Välkommen till min Github! https://github.com/Awreally ')
