a5abf9979c7b1efb790211bf1b44ccc376064b1a
Drilon Shehu <drilonshehu99@hotmail.com>
2025-10-20 00:52:44 +0200

    bild och knapp

---
+++ Drilonjs.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("#btn");
+btn.addEventListener("click", () => {
+});
+function alertFunction() {
+    alert("Du har tryckt på knappen!");
+}
+const btn = document.getElementById("#btn");
+btn.addEventListener("click", alertFunction);
+
+
+
+const img = document.createElement("img");
+img.src = "./assets/Bilder/Bildpamig.jpg";
+img.alt = "Drilon Shehu";
+img.classList.add("footer");
